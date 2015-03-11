8325dd17cf312d6aef55be34ae30a729c2b7b0ef
Denis Lamtsov <denisla@parallels.com>
2015-03-12 00:38:11 +0300

    Fix FOUR_CC macro

--- common.h
+++ common.h
@@ -6,12 +6,6 @@
 
 #define ASSERT(X) if(!(X)) { std::cout << #X << std::endl; std::abort(); }
 
-#define FOUR_CC(A, B, C, D) \
-	((((A) & 0xFF) << 24) |\
-	 (((B) & 0xFF) << 16) |\
-	 (((C) & 0xFF) <<  8) |\
-	 ( (D) & 0xFF))
-
 typedef unsigned int uint;
 
 #endif // _MPEG_COMMON_H_
