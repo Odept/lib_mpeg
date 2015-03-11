fb17965ac40f89ee62aa133a2ab79e73e0c9fb2d
Denis Lamtsov <denisla@parallels.com>
2015-03-11 04:13:11 +0300

    Separate header and file test cases in test

--- test.cpp
+++ test.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
+#include "common.h"
 
 #include "header.h"
+#include "stream.h"
 
 
-int main(int, char**)
+void test_header(uint f_val)
 {
-	CMPEGHeader h(0x40a0fbff);
+	CMPEGHeader h(f_val);
 
 	if(h.isValid())
-		std::cout << "Version:      2" << (h.getMpegVersion().isV2() ? "" : ".5") << std::endl <<
+		std::cout << "Header:       0x" << std::hex << f_val << std::dec << std::endl <<
+					 "Version:      2" << (h.getMpegVersion().isV2() ? "" : ".5") << std::endl <<
 					 "Layer:        " << h.getLayer() << std::endl <<
 					 "Bitrate:      " << h.getBitrate() << std::endl <<
 					 "Frequency:    " << h.getFrequency() << std::endl <<
@@ -25,6 +27,65 @@ int main(int, char**)
 					 "Frame length: " << h.getFrameLength() << std::endl;
 	else
 		std::cout << "Header is not valid" << std::endl;
+}
+
+
+void test_file(const char* f_path)
+{
+	FILE* f;
+
+	f = fopen(f_path, "rb");
+	if(!f)
+	{
+		std::cout << "Failed to open \"" << f_path << "\"" << std::endl;
+		return;
+	}
+
+	unsigned char* pBuf = NULL;
+	CMPEGStream* pMPEG = NULL;
+	do
+	{
+		fseek(f, 0, SEEK_END);
+		uint fsize = ftell(f);
+		rewind(f);
+
+		pBuf = new(std::nothrow) unsigned char[fsize];
+		if(!pBuf)
+		{
+			std::cout << "Failed to allocate " << fsize << " bytes" << std::endl;
+			break;
+		}
+
+		if(fread(pBuf, fsize, 1, f) != 1)
+		{
+			std::cout << "Failed to read " << fsize << " bytes" << std::endl;
+			break;
+		}
+
+		pMPEG = CMPEGStream::get(pBuf, fsize);
+		if(!pMPEG)
+		{
+			std::cout << "Failed to init MPEG stream" << std::endl;
+			break;
+		}
+
+		std::cout << f_path << std::endl << "================" << std::endl <<
+					 "Frames:  " << pMPEG->getFrameCount() << std::endl <<
+					 "Length:  " << pMPEG->getLength() << std::endl <<
+					 "Bitrate: " << pMPEG->getBitrate() << std::endl <<
+					 "Offset:  " << pMPEG->getFirstHeaderOffset() << std::endl;
+	}
+	while(0);
+
+	delete(pMPEG);
+	delete(pBuf);
+	fclose(f);
+}
+
+int main(int, char**)
+{
+	//test_header(0x44e0fbff);
+	test_file("test.mp3");
 
 	return 0;
 }
